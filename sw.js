b528966866ac4391a61d5cdfa6de95e023596716
Iosune Goñi <iosune@acornjuice.com>
2026-07-03 06:50:29 +0200

    Adding flow to request trial

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = 'accuvideo-v15';
+const CACHE_NAME = 'accuvideo-v18';
 const PRECACHE_URLS = [
   '/',
   '/index.html',
   '/thanks.html',
   '/assets/css/style.css?v=10',
-  '/assets/js/main.min.js?v=14',
-  '/assets/js/main.js?v=14',
+  '/assets/js/main.min.js?v=15',
+  '/assets/js/main.js?v=15',
   '/assets/img/AccuVideo_Logo.png',
   '/assets/img/favicon.ico',
   '/assets/videos/demo-poster.jpg',
