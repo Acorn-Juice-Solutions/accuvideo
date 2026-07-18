94efb816a0fb8c7a7668f56d0ac4e4ebc6499868
Iosune <37101496+Neonexus29@users.noreply.github.com>
2026-07-18 09:39:20 +0200

    Bigger text (#11)

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'accuvideo-v25';
+const CACHE_NAME = 'accuvideo-v26';
 const PRECACHE_URLS = [
   '/',
   '/index.html',
   '/thanks.html',
-  '/assets/css/style.css?v=16',
-  '/assets/js/main.min.js?v=23',
+  '/assets/css/style.css?v=17',
+  '/assets/js/main.min.js?v=24',
   '/assets/js/main.js?v=18',
   '/assets/img/AccuVideo_Logo.png',
   '/assets/img/favicon.ico',
